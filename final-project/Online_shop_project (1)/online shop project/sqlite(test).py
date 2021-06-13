845a3b01391a32a9714778df8acc2540cbdf09b3
AmiraliBisheban <82848349+AmiraliBisheban@users.noreply.github.com>
2021-06-14 03:05:24 +0430

    Delete final-project/Online_shop_project (1)/online shop project directory

--- final-project/Online_shop_project (1)/online shop project/sqlite(test).py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-a=str(input())
-#b="'"+a+"'"
-con = sqlite3.connect("mydatabase.db")
-cur = con.cursor()
-b=cur.execute(" SELECT password FROM customers WHERE username = " + a)
-for i in b:
-    print(i)
